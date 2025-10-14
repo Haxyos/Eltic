44aff0f01e62f264b1b7198b14f6bb12f3ca77ec
Willelm Delacoudre <delacoudrew@gmail.com>
2025-10-14 22:21:18 +0200

    (UPDATE) more training on vue

--- Eltic/src/App.vue
+++ Eltic/src/App.vue
@@ -2,6 +2,9 @@
   <h1>Eltic entreprise</h1>
   <p>Bienvenu :{{ firstName }}</p>
   <p>Compteur : {{ count }}</p>
+  <div v-bind:id="`p-${count}`">Div avec id dynamique</div>
+  <div v-bind:id="count + 1">Div avec autre id dynamique</div>
+  <div v-show="count >= 5">BRAVO !</div>
   <button v-on:click="increment()">Compteur</button>
   <button v-on:click="reset()">Reset</button>
 </template>
