85104347987af3fb5d8ce47ae627431f3ad14567
Willelm Delacoudre <delacoudrew@gmail.com>
2025-10-14 22:01:29 +0200

    (UPDATE) changing from react to vue and training on vue

---
+++ Eltic/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Eltic entreprise</h1>
+  <p>Bienvenu :{{ firstName }}</p>
+  <p>Compteur : {{ count }}</p>
+  <button v-on:click="increment()">Compteur</button>
+  <button v-on:click="reset()">Reset</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const firstName = 'Willelm'
+const count = ref(0)
+const increment = () => {
+  count.value++
+}
+
+const reset = () => {
+  count.value = 0
+}
+</script>
