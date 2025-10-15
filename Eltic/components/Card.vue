b81b36344a31f294e9b4253d945d02c19e22f26d
Haxyos <delacoudrew@gmail.com>
2025-10-15 14:55:43 +0200

    (ADD) creating card
    the cards are the main services that Eltic company can offer

--- Eltic/components/Card.vue
+++ Eltic/components/Card.vue
@@ -1,5 +1,25 @@
 <template>
-    <div class="flex w-11/12 content-center justify-center items-center shadow-xl drop-shadow-sm">
-        <div class="text-xl">Content</div>
+    <div>
+        <div v-for="(_,num) in listeService"
+            class="flex w-11/12 content-center justify-center items-center shadow-xl drop-shadow-sm">
+            <component class="text-xl" >Content</component>
+        </div>
     </div>
 </template>
+
+<script>
+import ElticBorne from '../src/ElticBorne.vue'
+import ElticVideo from '../src/ElticVideo.vue'
+
+const listeService = [
+    "ElticBorne",
+    "ElticVideo"
+]
+
+const listeServiceLibelle = [
+    "Eltic : Borne de recharge",
+    "Eltic : Video protection"
+]
+
+
+</script>
--- Eltic/src/App.vue
+++ Eltic/src/App.vue
@@ -1,28 +1,13 @@
 <template>
   <NavBar />
   <h1>Eltic entreprise</h1>
-  <p>Bienvenu :{{ firstName }}</p>
-  <p>Compteur : {{ count }}</p>
-  <div v-bind:id="`p-${count}`">Div avec id dynamique</div>
-  <div v-bind:id="count + 1">Div avec autre id dynamique</div>
-  <div v-show="count >= 5">BRAVO !</div>
-  <button v-on:click="increment()">Compteur</button>
-  <button v-on:click="reset()">Reset</button>
+  <Card/>
   <Footer/>
 </template>
 
 <script setup>
 import NavBar from '../components/Nav.vue'
 import Footer from '../components/Footer.vue'
-import { ref } from 'vue'
+import Card from '../components/Card.vue'
 
-const firstName = 'Willelm'
-const count = ref(0)
-const increment = () => {
-  count.value++
-}
-
-const reset = () => {
-  count.value = 0
-}
 </script>
