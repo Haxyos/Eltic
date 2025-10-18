ca26a8771e92330888dc28d965eac6f2cbbefa07
Haxyos <delacoudrew@gmail.com>
2025-10-19 01:18:36 +0200

    (UPDATE) manage to resolve the problem
    my value wasn't reactive so it couldn't update by itself but now it's fixed

--- Eltic/components/Card.vue
+++ Eltic/components/Card.vue
@@ -2,6 +2,7 @@
     <div class="flex content-center justify-center items-center">
         <div class="flex flex-col content-center justify-center items-center p-5 w-11/12">
             <button
+            @click="emission()"
                 class="flex flex-col w-11/12 content-center justify-center items-center shadow-xl drop-shadow-sm hover:cursor-pointer hover:scale-120 duration-300">
                 <h1 class="w-full text-center">{{ libelle }}</h1>
                 <img v-bind:src=image alt="Logo"></img>
@@ -14,10 +15,17 @@ export default {}
 
 <script setup>
 
-defineProps({
+const props = defineProps({
     title: String,
     libelle: String,
     image: String
 })
+
+const emits = defineEmits([
+    'true', 'false'
+])
+function emission(){
+    emits('true', props.title);
+}
 
 </script>
--- Eltic/src/App.vue
+++ Eltic/src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <div>
+  <div v-show="pageOn == false">
     <h1 class="text-center content-center font-bold text-6xl p-5">Eltic entreprise</h1>
   </div>
-  <Card v-for="(_,services) in listeService" :title="listeService[services]" :libelle="listeServiceLibelle[services]" :image="listeImageService[services]"/>
+  <Card v-if="pageOn == false" v-for="(_,services) in listeService" :key="services" :title="listeService[services]" :libelle="listeServiceLibelle[services]" :image="listeImageService[services]" @true="passePage(true)" @false="passePage(true)"/>
+  <NavBorne v-else/>
+  <button @click="passePage(false)"> test retour </button>
   <Footer />
 </template>
 
 <script setup>
+import {ref, watch} from 'vue'
 import Footer from '../components/Footer.vue'
 import Card from '../components/Card.vue'
+import NavBorne from '../components/NavBorne.vue'
 
 
 const listeService = [
@@ -25,6 +29,14 @@ const listeImageService = [
     "../eltic+voiture-electrique020.png",
     "#"
 ]
+
+let pageOn = ref(false);
+
+
+function passePage(val){
+  pageOn.value = val
+  console.log(pageOn)
+}
 
 
 </script>
