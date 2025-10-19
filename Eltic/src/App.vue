488b8c1c34b0960f6de08469bc1dd565003aa861
Willelm Delacoudre <delacoudrew@gmail.com>
2025-10-19 23:44:42 +0200

    (UPDATE) enhanced components
    enhanced the sitch of components and starting building the ElticBorne page

--- Eltic/src/App.vue
+++ Eltic/src/App.vue
@@ -2,45 +2,53 @@
   <div v-show="pageOn == false">
     <h1 class="text-center content-center font-bold text-6xl p-5">Eltic entreprise</h1>
   </div>
-  <Card v-if="pageOn == false" v-for="(_,services) in listeService" :key="services" :title="listeService[services]" :libelle="listeServiceLibelle[services]" :image="listeImageService[services]" @true="passePage(true)" @false="passePage(true)"/>
-  <NavBorne v-else/>
-  <button @click="passePage(false)"> test retour </button>
+  <Card
+    v-if="pageOn == false"
+    v-for="(_, services) in listeService"
+    :key="services"
+    :title="listeService[services]"
+    :libelle="listeServiceLibelle[services]"
+    :image="listeImageService[services]"
+    @true="(p) => passePage(true, p)"
+    @false="(p) => passePage(false, p)"
+  />
+  <div v-else-if="ServiceActuelle == 'ElticBorne'">
+    <NavBorne />
+    <ElticBorne />
+  </div>
+  <div v-else-if="ServiceActuelle == 'ElticVideo'">
+    <NavVideo />
+    <ElticVideo />
+  </div>
+  <button @click="passePage(false)">test retour</button>
   <Footer />
 </template>
 
 <script setup>
-import {ref, watch} from 'vue'
+import { ref } from 'vue'
 import Footer from '../components/Footer.vue'
 import Card from '../components/Card.vue'
 import NavBorne from '../components/NavBorne.vue'
+import NavVideo from '../components/NavVideo.vue'
+import ElticBorne from '../components/ElticBorne.vue'
+import ElticVideo from '../components/ElticVideo.vue'
 
+let ServiceActuelle = ''
 
-const listeService = [
-    'ElticBorne',
-    'ElticVideo'
-]
+const listeService = ['ElticBorne', 'ElticVideo']
 
-const listeServiceLibelle = [
-    "Eltic : Borne de recharge",
-    "Eltic : Video protection"
-]
+const listeServiceLibelle = ['Eltic : Borne de recharge', 'Eltic : Video protection']
 
-const listeImageService = [
-    "../eltic+voiture-electrique020.png",
-    "#"
-]
+const listeImageService = ['../eltic+voiture-electrique020.png', '#']
 
-let pageOn = ref(false);
+let pageOn = ref(false)
 
-
-function passePage(val){
+function passePage(val, val2) {
   pageOn.value = val
-  console.log(pageOn)
+  ServiceActuelle = val2
 }
-
-
 </script>
 
 <style>
-@import "tailwindcss";
+@import 'tailwindcss';
 </style>
