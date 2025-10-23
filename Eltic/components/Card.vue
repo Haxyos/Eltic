00e2566f1dc3ac2be1524d392788f11509fddb67
Haxyos <delacoudrew@gmail.com>
2025-10-23 20:21:59 +0200

    (FIXES) correcting a lil bug

--- Eltic/components/Card.vue
+++ Eltic/components/Card.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="flex group content-center justify-center items-center">
+    <div class="flex content-center justify-center items-center">
         <div class="flex flex-col content-center justify-center items-center p-5 w-11/12">
             <button
             @click="emission()"
-                class="flex flex-col w-11/12 content-center justify-center items-center shadow-xl drop-shadow-sm rounded-2xl overflow-hidden hover:cursor-pointer hover:scale-105 duration-300">
+                class="flex flex-col group w-11/12 content-center justify-center items-center shadow-xl drop-shadow-sm rounded-2xl overflow-hidden hover:cursor-pointer hover:scale-105 duration-300">
                 <h1 class="w-full text-center text-3xl font-bold z-50">{{ libelle }}</h1>
                 <img :class="{'absolute left-0 transform transition-none z-0 opacity-45 group-hover:transition-transform group-hover:duration-1000 group-hover:ease-in group-hover:translate-x-[1500px]': title === 'ElticBorne'}" class="" v-bind:src=image alt="Logo"></img>
                 <div v-html="description" class="text-lg p-5 z-50 w-5/6"></div>
