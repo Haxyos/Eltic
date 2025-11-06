68be23d0c74dcba3c2b0e874986a2d73a6f12ccb
Willelm Delacoudre <delacoudrew@gmail.com>
2025-11-07 00:20:27 +0100

    (FIXES) fixing bug

--- Eltic/components/Footer.vue
+++ Eltic/components/Footer.vue
@@ -2,10 +2,10 @@
   <div class="w-full bg-[#41b6b3] text-white bottom-0">
     <div class="grid grid-cols-12 grid-rows-6 gap-2">
     <h1 class="text-left row-span-1 col-span-4 text-2xl">Contact commercial et technique :</h1>
-    <img class="row-span-3 col-span-1 row-start-2 w-25 h-25" src="../eltic_IRVE_contact1a.gif" alt="0607288720 bornes-de-recharge@eltic.fr"></img>
+    <img class="row-span-3 col-span-1 row-start-2 w-25 h-25" src="../public/eltic_IRVE_contact1a.gif" alt="0607288720 bornes-de-recharge@eltic.fr"></img>
     <div class="row-start-2 col-start-2 col-span-2">06 07 28 87 20</div>
     <div class="row-start-3 col-start-2 col-span-3">bornes-de-recharge@eltic.fr</div>
-    <img class="row-start-5 row-span-2" src="../eltic_logo063.gif" alt="Eltic"></img>
+    <img class="row-start-5 row-span-2" src="../public/eltic_logo063.gif" alt="Eltic"></img>
     <div class="row-start-5 row-span-2 col-start-2 col-span-full"> sarl au capital de 8 000 euros  -  RCS Rennes  -  35510 Cesson‑Sévigné  -  France  -  Mentions légales  -  © eltic 2025 </div>
   
     </div>
